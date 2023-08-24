2d185087c80ec433f684c6328a8bbaf87db916dd
Paul Hamrick <hamri@unc.edu>
2023-08-24 10:44:56 -0400

    Creating Hello, World! File

---
+++ lessons/input.py
@@ -0,0 +1,5 @@
+"""Demonstrates asking the user for input"""
+
+username : str = input("What is your name? ")
+print("Hello, " + username + " good morning!")
+print("You are the best programmer ever," + username)
