6b8b9d41489eff329f84c26458361adc98c7f3c3
Paul Hamrick <hamri@unc.edu>
2023-09-01 15:48:23 -0400

    Asking for information; Part 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,7 @@
+"""Chardle Assignment: Coding Part of Wordle"""
+
+__author__ = "730556372"
+
+word : str = input("Enter a 5-character word: ") # Grabbing the word
+letter : str = input("Enter a single character: ") # Grabbing the letter
+print("Searching for " + letter + " in " + word)
