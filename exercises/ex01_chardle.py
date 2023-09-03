be09aaa44441e665fb0f1b29ba4c9362b375604d
Paul Hamrick <hamri@unc.edu>
2023-09-03 17:13:01 -0400

    Finding matches; Part 2

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -5,3 +5,18 @@ __author__ = "730556372"
 word : str = input("Enter a 5-character word: ") # Grabbing the word
 letter : str = input("Enter a single character: ") # Grabbing the letter
 print("Searching for " + letter + " in " + word)
+
+if word[0] == letter:
+    print("letter" + " found at index 0")
+
+if word[1] == letter:
+    print("letter" + " found at index 1")
+
+if word[2] == letter:
+    print("letter" + " found at index 2")
+
+if word[3] == letter:
+    print("letter" + " found at index 3")
+
+if word[4] == letter:
+    print("letter" + " found at index 4")
