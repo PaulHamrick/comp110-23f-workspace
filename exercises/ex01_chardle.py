bc9fbd011486083a7c071aa915fb825a2ef3bc08
Paul Hamrick <hamri@unc.edu>
2023-09-03 17:29:46 -0400

    Finished Code

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -3,11 +3,15 @@
 __author__ = "730556372"
 
 word : str = input("Enter a 5-character word: ") # Grabbing the word
-if len(word) != 5:
+if len(word) != 5: # Code if the word is not the right length
     print("Error: Word must contain 5 characters")
     exit()
 
 letter : str = input("Enter a single character: ") # Grabbing the letter
+if len(letter) != 1: # Code if the letter is not one letter
+    print("Error: Character must be a single character.")
+    exit()
+
 instances : int = 0 # Creating a variable to track instances of the letter
 print("Searching for " + letter + " in " + word)
 
