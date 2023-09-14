f243d04c70540be537ecb3279021f4113d4de9ce
Paul Hamrick <hamri@unc.edu>
2023-09-14 11:55:04 -0400

    Finalized code

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,11 +2,11 @@
 
 __author__ = "730556372"
 
-secret: str = "python" #Choosing the secret word
-secret_length: int = len(secret) # Making sure the code does not break 
-                           # If the secret word has a different length
+secret: str = "python"  # Choosing the secret word
+secret_length: int = len(secret)  # Making sure the code does not break 
+# If the secret word has a different length
 guess: str = input(f"What is your { secret_length }-letter guess? ") 
-                                 # Getting an initial guess
+# Getting an initial guess
 
 # Storing the different emojis that will be output
 WHITE_BOX: str = "\U00002B1C"
@@ -16,10 +16,10 @@ YELLOW_BOX: str = "\U0001F7E8"
 output_boxes: str = ""
 string_idx: int = 0
 does_letter_appear_elsewhere: bool = False
-yellow_string_idx: int = 0 #String index to figure out if a box should be yellow
+yellow_string_idx: int = 0  # String index to figure out if a box should be yellow
 
 # I can't figure out how to merge this and the repeated block later
-if (len(guess) == secret_length): # If the guess is the right length
+if (len(guess) == secret_length):  # If the guess is the right length
     while (string_idx < secret_length): 
         # Iterating over each character of the strings
         if (guess[string_idx] == secret[string_idx]):
@@ -27,12 +27,12 @@ if (len(guess) == secret_length): # If the guess is the right length
         else:
             yellow_string_idx = 0
             does_letter_appear_elsewhere = False
-            while (yellow_string_idx < secret_length): # Iterating over possible indices of the secret word
-                if (guess[string_idx] == secret[yellow_string_idx]): #Checking if the secret word matches the guess
+            while (yellow_string_idx < secret_length):  # Iterating over possible indices of the secret word
+                if (guess[string_idx] == secret[yellow_string_idx]):  # Checking if the secret word matches the guess
                     does_letter_appear_elsewhere = True
                 yellow_string_idx += 1
             # Adding whichever of YELLOW_BOX and WHITE_BOX is appropriate
-            if (does_letter_appear_elsewhere == True):
+            if does_letter_appear_elsewhere:
                 output_boxes += YELLOW_BOX
             else:
                 output_boxes += WHITE_BOX
@@ -40,16 +40,14 @@ if (len(guess) == secret_length): # If the guess is the right length
     print(output_boxes)
     if (guess == secret):
         print("Woo! You got it!")
-        exit() # Ending the program
     else:
         print("Not quite. Play again soon!")
-        exit() # Ending the program
 
 while (len(guess) != secret_length):
     guess = input(f"That was not { secret_length } letters! Try again: ")
-                                      # Getting a new guess
-    if (len(guess) == secret_length): # If the guess is the right length
-        while (string_idx < secret_length): # The same thing as before
+    # Getting a new guess
+    if (len(guess) == secret_length):  # If the guess is the right length
+        while (string_idx < secret_length):  # The same thing as before
             if (guess[string_idx] == secret[string_idx]):
                 output_boxes += GREEN_BOX
             else:
@@ -59,7 +57,7 @@ while (len(guess) != secret_length):
                     if (guess[string_idx] == secret[yellow_string_idx]):
                         does_letter_appear_elsewhere = True
                     yellow_string_idx += 1
-                if (does_letter_appear_elsewhere == True):
+                if does_letter_appear_elsewhere:
                     output_boxes += YELLOW_BOX
                 else:
                     output_boxes += WHITE_BOX
@@ -67,7 +65,5 @@ while (len(guess) != secret_length):
         print(output_boxes)
         if (guess == secret):
             print("Woo! You got it!")
-            exit()
         else:
             print("Not quite. Play again soon!")
-            exit()
