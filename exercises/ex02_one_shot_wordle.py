648dab87653c32b60f5945d3d81f78d3f3cc978c
Paul Hamrick <hamri@unc.edu>
2023-09-14 11:09:48 -0400

    Part 2 and a bit of Part 3

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -8,7 +8,35 @@ secret_length: int = len(secret) # Making sure the code does not break
 guess: str = input(f"What is your { secret_length }-letter guess? ") 
                                  # Getting an initial guess
 
+# Storing the different emojis that will be output
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+# Initializing the output boxes and the string's index
+output_boxes: str = ""
+string_idx: int = 0
+does_letter_appear_elsewhere: bool = False
+yellow_string_idx: int = 0 #String index to figure out if a box should be yellow
+
+# I can't figure out how to merge this and the repeated block later
 if (len(guess) == secret_length): # If the guess is the right length
+    while (string_idx < secret_length): 
+        # Iterating over each character of the strings
+        if (guess[string_idx] == secret[string_idx]):
+            output_boxes += GREEN_BOX
+        else:
+            yellow_string_idx = 0
+            does_letter_appear_elsewhere = False
+            while (yellow_string_idx < secret_length):
+                if (guess[string_idx] == secret[yellow_string_idx]):
+                    does_letter_appear_elsewhere = True
+                yellow_string_idx += 1
+            if (does_letter_appear_elsewhere == True):
+                output_boxes += YELLOW_BOX
+            else:
+                output_boxes += WHITE_BOX
+        string_idx += 1
+    print(output_boxes)
     if (guess == secret):
         print("Woo! You got it!")
         exit() # Ending the program
@@ -20,6 +48,13 @@ while (len(guess) != secret_length):
     guess = input(f"That was not { secret_length } letters! Try again: ")
                                       # Getting a new guess
     if (len(guess) == secret_length): # If the guess is the right length
+        while (string_idx < secret_length): # The same thing as before
+            if (guess[string_idx] == secret[string_idx]):
+                output_boxes += GREEN_BOX
+            else:
+                output_boxes += WHITE_BOX
+            string_idx += 1
+        print(output_boxes)
         if (guess == secret):
             print("Woo! You got it!")
             exit()
