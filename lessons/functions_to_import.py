80e0c590855098aebb08e09ea3d66b6d30a61f0e
Paul Hamrick <hamri@unc.edu>
2023-09-28 10:51:26 -0400

    Creating the file

---
+++ lessons/functions_to_import.py
@@ -0,0 +1,6 @@
+"""Here are some functions and some code to test importing."""
+
+def blah(words: str) -> str:
+    return words + words + words + words
+
+print(__name__)
---
+++ lessons/importing.py
@@ -0,0 +1,3 @@
+"""Testing the properties of import in Python."""
+
+from lessons import functions_to_import
