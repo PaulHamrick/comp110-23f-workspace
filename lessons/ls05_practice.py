305109128b13048dec9bc66099a714f6c67e9ec8
Paul Hamrick <hamri@unc.edu>
2023-10-15 13:31:52 -0400

    Setting up the file

--- lessons/ls05_practice.py
+++ lessons/ls05_practice.py
@@ -1,6 +1,2 @@
-my_number_string : str = input("Guess a number: ")
-my_number : int = int(my_number_string)
-if my_number == 10:
-    print("Right")
-else:
-    print("Wrong")
+some_list: list[str] = ["apples", "eggs"]
+print(some_list)
