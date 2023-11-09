8503cffb5a97f8f457c01cebb44a7b553c62e7eb
Paul Hamrick <hamri@unc.edu>
2023-11-09 10:12:36 -0500

    Modifying point file

--- lessons/CQ07/point.py
+++ lessons/CQ07/point.py
@@ -7,7 +7,7 @@ class Point:
     x: float
     y: float
     
-    def __init__(self, x_init: float, y_init: float):
+    def __init__(self, x_init: float = 0.0, y_init: float = 0.0):
         """My constructor."""
         self.x = x_init
         self.y = y_init
@@ -21,5 +21,21 @@ class Point:
         """Creating a new scaled point."""
         new_point: Point = Point(self.x * factor, self.y * factor)
         return new_point
+    
+    def __str__(self) -> str:
+        """Writing the point as a string."""
+        point_string: str = f"x: {self.x}; y: {self.y}"
+        return point_string
+    
+    def __mul__(self, factor: int | float) -> Point:
+        """Creating a new scaled point with the * operator."""
+        new_point: Point = Point(self.x * factor, self.y * factor)
+        return new_point
+    
+    def __add__(self, addend: int | float) -> Point:
+        """Creating a new point with something added to the x and y coordinates."""
+        new_point: Point = Point(self.x + addend, self.y + addend)
+        return new_point
+
 
         
--- lessons/message.py
+++ lessons/message.py
@@ -2,5 +2,35 @@
 
 class Message:
 
-    to: str
-    text: str
+    to: str | int
+    content: str
+    important: bool
+
+    def __init__(self, recipient: str | int, message_content: str, importance_flag: bool):
+        """Construct a message."""
+        self.to = recipient
+        self.content = message_content
+        self.important = importance_flag
+
+    def __str__(self) -> str:
+        output: str = f"Message to {self.to}:\n"
+        output += f"Important? {self.important}\n"
+        output += f'"{self.content}"'
+        return output
+    
+    def __mul__(self, factor: int):
+        """Multiplication operator overload."""
+        copy_val: str = self.content
+        for loop_number in range(1, factor):
+            self.content += " " + copy_val
+
+
+
+msg: Message = Message("Erin", "Great job!", False)
+msg * 5
+print(msg)
+
+def add(x: int | float, y: int | float = 1) -> int | float:
+    return x + y
+
+print(add(1, 0.25))
